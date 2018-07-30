9385df5f28c68410b8db7450fa8707eefd4ac7b5
kurbanovfarkhad <deguar12@gmail.com>
2018-07-30 23:04:20 +0600

    backend development

--- src/main/java/com/example/Authentication/controllers/MainController.java
+++ src/main/java/com/example/Authentication/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.example.Authentication.controllers;
 
+import com.example.Authentication.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String main(){
-
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
         return "index";
     }
 }
